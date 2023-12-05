54eec261e2dc55d768362a1a86c7b3444aac3fe1
Z-iutek <127032295+Z-iutek@users.noreply.github.com>
2023-12-05 18:54:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,23 @@
+const gridElement = document.getElementById('grid');
+let activeSquare = null;
+
 // Utwórz tablicę 2D do śledzenia indeksów kwadratów
 const squares = [];
 for (let i = 0; i < 3; i++) {
   squares[i] = [];
-}
-
-const gridElement = document.getElementById('grid');
-let activeSquare = null;
-let score = 0;
-
-for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 3; j++) {
     const square = document.createElement('div');
     square.classList.add('square');
-    const randomNumber = getRandomNumber(-10, 10);
-    square.textContent = randomNumber;
-    square.dataset.value = randomNumber;
+    square.textContent = i * 3 + j + 1;
 
     square.addEventListener('click', () => {
-      handleSquareClick(square);
+      setActiveSquare(square);
     });
 
     gridElement.appendChild(square);
     squares[i][j] = square;
   }
 }
-
-// Startowo zaznacz pole środkowe
-setActiveSquare(squares[1][1]);
-
-// Dodaj pole użytkowe "HERO"
-const heroSquare = document.createElement('div');
-heroSquare.classList.add('square', 'hero');
-heroSquare.textContent = 'HERO';
-gridElement.appendChild(heroSquare);
 
 // Funkcja ustawiająca aktywny kwadrat
 function setActiveSquare(square) {
@@ -44,27 +29,51 @@ function setActiveSquare(square) {
   activeSquare.classList.add('active');
 }
 
-// Funkcja obsługująca kliknięcie na kwadrat
-function handleSquareClick(square) {
-  if (square === heroSquare) {
-    // Nie dodawaj/odejmuj punktów za kliknięcie w pole "HERO"
-    return;
+// Obsługa strzałek
+document.addEventListener('keydown', (event) => {
+  if (activeSquare) {
+    const currentIndex = getIndex(activeSquare);
+
+    let newIndex;
+    switch (event.key) {
+      case 'ArrowUp':
+        newIndex = moveIndex(currentIndex, -1, 0);
+        break;
+      case 'ArrowDown':
+        newIndex = moveIndex(currentIndex, 1, 0);
+        break;
+      case 'ArrowLeft':
+        newIndex = moveIndex(currentIndex, 0, -1);
+        break;
+      case 'ArrowRight':
+        newIndex = moveIndex(currentIndex, 0, 1);
+        break;
+      default:
+        return;
+    }
+
+    if (isValidIndex(newIndex)) {
+      setActiveSquare(squares[newIndex.row][newIndex.col]);
+    }
   }
+});
 
-  const value = parseInt(square.dataset.value, 10);
-  score += value;
-
-  updateScore();
-  setActiveSquare(square);
+// Funkcja do uzyskania indeksu kwadratu w tablicy
+function getIndex(square) {
+  for (let i = 0; i < squares.length; i++) {
+    const j = squares[i].indexOf(square);
+    if (j !== -1) {
+      return { row: i, col: j };
+    }
+  }
 }
 
-// Funkcja aktualizująca wynik na stronie
-function updateScore() {
-  const scoreElement = document.getElementById('score');
-  scoreElement.textContent = `Wynik: ${score}`;
+// Funkcja do przesuwania indeksu
+function moveIndex(index, rowOffset, colOffset) {
+  return { row: index.row + rowOffset, col: index.col + colOffset };
 }
 
-// Funkcja generująca losową liczbę z zakresu
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+// Funkcja sprawdzająca, czy nowy indeks jest poprawny
+function isValidIndex(index) {
+  return index.row >= 0 && index.row < 3 && index.col >= 0 && index.col < 3;
 }
