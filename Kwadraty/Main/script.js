37096acbcf6136b91dc4098b2b5ac62456aad5d8
TadeuszMorg <105056537+ahiklo@users.noreply.github.com>
2023-12-16 14:25:50 +0100

    Update script.js

--- Kwadraty/Main/script.js
+++ Kwadraty/Main/script.js
@@ -1,18 +1,26 @@
-//Wyświetlanie w kosoli jaki obrazek został wybrany początkowy
-document.addEventListener('DOMContentLoaded', function() {
-  // Pobierz zmienną z localStorage
-  const ostatniWybranyObrazek = localStorage.getItem('ostatniWybranyObrazek');
-  
-  // Sprawdź, czy zmienna istnieje i wyświetl w konsoli
-  if (ostatniWybranyObrazek !== null) {
-    console.log('Ostatni wybrany obrazek:', ostatniWybranyObrazek);
+const skill = localStorage.getItem('ostatniWybranyObrazek');
+console.log(skill + ' to numer');
+
+// Umiejki -----------------------------------------------------------------------------------------------------------------------------------
+function decreaseNeighbourHP() {
+  for (let i = 0; i <= 8; i++){
+  const neighbourIndex = currentCellIndex - (i-4); // Komórka po lewej stronie (jeśli istnieje)
+  const highlightedRow = Math.floor(currentCellIndex / 5);
+  const currentRow = Math.floor((currentCellIndex - (i-4)) / 5);
+  if (highlightedRow === currentRow) {
+    const neighbourCellValues = cells[neighbourIndex].textContent.split('-');
+    let neighbourHP = parseInt(neighbourCellValues[0]);
+
+     if (neighbourHP <= 15){
+        neighbourHP = 0;
+     } else{ 
+        neighbourHP -= 15;
+     }
+    cells[neighbourIndex].textContent = generateCellValue(neighbourHP, parseInt(neighbourCellValues[1]), parseInt(neighbourCellValues[2]));
   } else {
-    console.log('Nie wybrano jeszcze obrazka.');
+    console.log('FUCK U BITCH')
   }
-});
-
-
-
+}}
 
 
 
@@ -38,6 +46,22 @@ let steps = 0;
 let oblicz = 0;
 let armor = 0;
 
+if (skill == 1)        {
+  maxHP = 8;
+  initialHP = 8;
+  initialDMG = 14;
+} else if (skill == 2) {
+  maxHP = 10;
+  initialHP = 10;
+  initialDMG = 12;
+  armor = 1;
+} else if (skill == 3) {
+  maxHP = 12;
+  initialHP = 12;
+  initialDMG = 10;
+  armor = 2;
+}
+
 
 
 // Tworzenie siatki komórek z wartościami początkowymi
@@ -52,9 +76,9 @@ for (let i = 0; i < 5; i++) {
       cell.classList.add('highlighted');
     } else {
       cell.textContent = generateCellValue(
-        Math.floor(Math.random() * 15 + 1),
-        Math.floor(Math.random() * 15 + 1),
-        Math.floor(Math.random() * 5 + 1)
+        Math.floor(0),
+        Math.floor(0),
+        Math.floor(Math.random() * 4 + 2)
       );
     }
     grid.appendChild(cell);
@@ -76,11 +100,43 @@ function checkStats() {
       let cellMoney = parseInt(cellValues[2]);
 
 
-
       // WAŻNE ----------------- WAŻNE - To pozwala manipulować wartościami HP-ATAK-MONEY istniejących komórek zależnie od czegokolwiek
-      if (cellMoney <= 2) {
-        cellDMG = 0;
-        cellHP = 0;
+      if (cellMoney == 3 && cellHP == 0 && cellDMG == 0) {
+        cellDMG = 4;
+        cellHP = 8;
+      } else if (cellMoney == 4 && cellHP == 0 && cellDMG == 0) {
+        cellDMG = 4;
+        cellHP = 16;
+      } else if (cellMoney == 5 && cellHP == 0 && cellDMG == 0) {
+        cellDMG = 3;
+        cellHP = 14;
+      } else if (cellMoney == 6 && cellHP == 0 && cellDMG == 0) {
+        cellDMG = 4;
+        cellHP = 17;
+      } else if (cellMoney == 7 && cellHP == 0 && cellDMG == 0) {
+        cellDMG = 1;
+        cellHP = 20;
+      } else if (cellMoney == 8 && cellHP == 0 && cellDMG == 0) {
+        cellDMG = 8;
+        cellHP = 11;
+      } else if (cellMoney == 9 && cellHP == 0 && cellDMG == 0) {
+        cellDMG = 3;
+        cellHP = 15;
+      } else if (cellMoney == 10 && cellHP == 0 && cellDMG == 0) {
+        cellDMG = 4;
+        cellHP = 20;
+      } else if (cellMoney == 11 && cellHP == 0 && cellDMG == 0) {
+        cellDMG = 10;
+        cellHP = 41;
+      }
+      cell.textContent = generateCellValue(cellHP, cellDMG, parseInt(cellValues[2]));
+
+
+
+
+      // Tu unikalne efekty komórek
+      if (cellMoney == 11 && cellHP >= 2){
+        cellHP -=1;
         cell.textContent = generateCellValue(cellHP, cellDMG, parseInt(cellValues[2]));
       }
     }
@@ -93,22 +149,48 @@ checkStats();
 // Nasłuchiwanie na zdarzenie naciśnięcia klawisza
 document.addEventListener('keydown', (event) => {
 
+  
   // Pobranie wartości z komórki "highlighted" i zapisanie ich w zmiennych
   const highlightedValues = cells[currentCellIndex].textContent.split('-');
   let highlightedHP = parseInt(highlightedValues[0]);
   let highlightedDMG = parseInt(highlightedValues[1]);
   let highlightedMoney = parseInt(highlightedValues[2]);
 
-  console.log('Wartości z komórki highlighted:', highlightedHP, highlightedDMG, highlightedMoney);
+  console.log('Wartości z komórki highlighted:', highlightedMoney);
 
-  // To zmioenia poprzednio okupowaną komórke na losową
-  cells[currentCellIndex].textContent = generateCellValue(
-    Math.floor(Math.random() * 15 + 1),
-    Math.floor(Math.random() * 15 + 1),
-    Math.floor(Math.random() * 5 + 1)
-  );
+
+    // To zmioenia poprzednio okupowaną komórke na losową
+    if (highlightedMoney <= 100)        {
+      cells[currentCellIndex].textContent = generateCellValue(
+        Math.floor(0),
+        Math.floor(0),
+        Math.floor(Math.random() * 10 + 1)
+      );
+    } else if (highlightedMoney <= 200) {
+      cells[currentCellIndex].textContent = generateCellValue(
+        Math.floor(0),
+        Math.floor(0),
+        Math.floor(Math.random() * 15 + 1)
+      );
+    } else if (highlightedMoney <= 300) {
+      cells[currentCellIndex].textContent = generateCellValue(
+        Math.floor(0),
+        Math.floor(0),
+        Math.floor(Math.random() * 20 + 1)
+      );
+    } else if (highlightedMoney >= 301) {
+      cells[currentCellIndex].textContent = generateCellValue(
+        Math.floor(0),
+        Math.floor(0),
+        Math.floor(Math.random() * 20 + 6)
+      );
+    }
+  
+  
+
+
   cells[currentCellIndex].classList.remove('highlighted'); // Usunięcie klasy 'highlighted' z poprzedniej komórki
-  checkStats();
+  
 
   
 
@@ -118,18 +200,32 @@ document.addEventListener('keydown', (event) => {
     currentCellIndex -= 1,
     steps += 1,
     oblicz += 1;
+    checkStats();
   } else if ((event.key === 'w' || event.key === 'ArrowUp') && currentCellIndex >= 5) {
     currentCellIndex -= 5,
     steps += 1,
     oblicz += 1;
+    checkStats();
   } else if ((event.key === 'd'  || event.key==='ArrowRight') && currentCellIndex % 5 !== 4) {
     currentCellIndex += 1,
     steps += 1,
     oblicz += 1;
+    checkStats();
   } else if ((event.key === 's' || event.key==='ArrowDown')  && currentCellIndex < 20) {
     currentCellIndex += 5,
     steps += 1,
     oblicz += 1;
+    checkStats();
+  } // Umiejki dla postaci
+    else if (event.key === 'r' && steps >= 1) {
+      if (skill == 1)        {
+        decreaseNeighbourHP();
+      } else if (skill == 2) {
+
+      } else if (skill == 3) {
+        
+      }
+      steps = 0;
   }
   console.log('Liczba kroków: ' + steps)
   console.log('Oblicz? ' + oblicz)
@@ -148,11 +244,9 @@ let currentMoney = parseInt(currentCellValues[2]);
 if (oblicz>=1) {
   if (currentMoney==1){
     highlightedHP +=1;
-  }
-else if (currentMoney==2){
+  } else if (currentMoney==2){
   highlightedHP +=2;
-}
-
+  } 
 
 
 // Tu jak to przeciwnik a nie specjalna karta
@@ -188,8 +282,8 @@ if (highlightedHP > maxHP) {
 cells[currentCellIndex].classList.add('highlighted'); // Dodanie klasy 'highlighted' do aktualnej komórki
 cells[currentCellIndex].textContent = generateCellValue(highlightedHP, highlightedDMG, highlightedMoney);
 
-// Wyświetlanie statystyk obok pola gry
 
+// Wyświetlanie statystyk obok pola ------------------------------------------------------------------------------------------------------------------
 function updateStats() {
   const hpValue = document.getElementById('hpValue');
   const dmgValue = document.getElementById('dmgValue');
@@ -205,7 +299,6 @@ function updateStats() {
   moneyValue.textContent = highlightedMoney;
   armorValue.textContent = armor;     // <---- do zmiany jak będzie aktywnie zmieniany
 }
-
 updateStats();
 
 
@@ -215,7 +308,10 @@ console.log('Zapisano wartość do Local Storage:', highlightedMoney); // <-- sp
 if(highlightedHP <= 0){  // <-- Sprawdzenie czy gracz nie przegrał
   GameOver() 
 }
+
+
 });
+
 
 
 function GameOver() {
